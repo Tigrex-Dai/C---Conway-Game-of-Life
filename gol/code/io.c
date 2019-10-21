641edcf4cc7c87eb99321c682b0708e4c0cb134e
y.dai <y.dai@>
2019-10-21 08:13:17 +0200

    update io

---
+++ gol/code/io.c
@@ -0,0 +1,79 @@
+#include "io.h"
+
+
+void affiche_trait (int c){
+	int i;
+	for (i=0; i<c; ++i) printf ("|---");
+	printf("|\n");
+	return;
+}
+
+void affiche_ligne (int c, int* ligne){
+	int i;
+	for (i=0; i<c; ++i) 
+		if (ligne[i] == 0 ) printf ("|   "); else printf ("| O ");
+	printf("|\n");
+	return;
+}
+
+void affiche_grille (grille g){
+	int i, l=g.nbl, c=g.nbc;
+	printf("\n");
+	affiche_trait(c);
+	for (i=0; i<l; ++i) {
+		affiche_ligne(c, g.cellules[i]);
+		affiche_trait(c);
+	}	
+	printf("\n"); 
+	return;
+}
+
+void efface_grille (grille g){
+	printf("\n\e[%dA",g.nbl*2 + 5); 
+}
+
+void debut_jeu(grille *g, grille *gc){
+	int vieillissement = 0;
+	char c = getchar(); 
+	while (c != 'q') // touche 'q' pour quitter
+	{ 
+		switch (c) {
+			case '\n' : 
+			{ // touche "entree" pour évoluer
+				evolue(g,gc);
+				efface_grille(*g);
+				affiche_grille(*g);
+				break;
+			}
+			case 'n' :
+			{ // touche "n" pour renouveller grille
+				char grille[100];
+                printf("\r\e[0KNouvelle grille a charger : \n");
+                scanf("%s", grille);
+
+				libere_grille(g);
+				libere_grille(gc);
+
+				init_grille_from_file(grille, g);
+				alloue_grille(g->nbl, g->nbc, gc);
+                  
+				temps_evolution = 0;
+				affiche_grille(*g);
+				break;				
+			}
+			case 'v' :
+			{ // touche "v" pour activer vieillissement
+				vieillissement += 1;
+				vieillissement %= 2;
+				break;
+			}
+			default : 
+			{ // touche non traitée
+				printf("\n\e[1A");
+				break;
+			}
+		}
+		c = getchar(); 
+	}
+	return;	
+}
---
+++ gol/code/jeu.c
@@ -0,0 +1,48 @@
+#include "jeu.h"
+
+int compte_voisins_vivants (int i, int j, grille g){
+	int v = 0, l=g.nbl, c = g.nbc;
+	v+= est_vivante(modulo(i-1,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i-1,l),modulo(j,c),g);
+	v+= est_vivante(modulo(i-1,l),modulo(j+1,c),g);
+	v+= est_vivante(modulo(i,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i,l),modulo(j+1,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j-1,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j,c),g);
+	v+= est_vivante(modulo(i+1,l),modulo(j+1,c),g);
+
+	return v; 
+}
+
+int compte_voisins_vivants_nc(int i, int j, grille g) {
+  int v = 0;
+	for(int k = max(0,i-1);k <= min(g.nbl-1,i+1);k++){
+		for(int l = max(0,j-1);l <= min(g.nbc-1,j+1);l++){
+			if((k != i || l != j) && est_vivante(k,l,g)){
+				v++;			
+			}
+		}
+	}
+  return v;
+}
+
+void evolue (grille *g, grille *gc){
+	copie_grille (*g,*gc); // copie temporaire de la grille
+	int i,j,l=g->nbl, c = g->nbc,v;
+	for (i=0; i<l; i++)
+	{
+		for (j=0; j<c; ++j)
+		{
+			v = compte_voisins_vivants (i, j, *gc);
+			if (est_vivante(i,j,*g)) 
+			{ // evolution d'une cellule vivante
+				if ( v!=2 && v!= 3 ) set_morte(i,j,*g);
+			}
+			else 
+			{ // evolution d'une cellule morte
+				if ( v==3 ) set_vivante(i,j,*g);
+			}
+		}
+	}
+	return;
+}
